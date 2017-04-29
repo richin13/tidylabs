7fdbe77a16a219422a32638b77668390dc95aa19
Ricardo Madriz <richin13@gmail.com>
2017-04-28 20:54:43 -0600

    Integrate devise with Tydilabs

--- app/controllers/defaults_controller.rb
+++ app/controllers/defaults_controller.rb
@@ -1,4 +1,6 @@
 class DefaultsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  # get 'defaults/index'
-
   root 'defaults#index'
+
+  devise_for :users, skip: [:sessions]
+  as :user do
+    get 'ingreso', to: 'devise/sessions#new', as: :new_user_session
+    post 'ingreso', to: 'devise/sessions#create', as: :user_session
+    delete 'salir', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
