f04405e7c99ab5a3bacae641462e5f2cdaeb4538
Ricardo Madriz <richin13@gmail.com>
2017-04-29 10:47:28 -0600

    Remove users namespace from login and logout urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'defaults#index'
-
-  devise_for :users
+  
+  devise_for :users, skip: [:sessions]
+  as :user do
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
 end
