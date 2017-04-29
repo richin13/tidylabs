ffc31afd9505d0907375664571e93da9c718c409
Dascr32 <dasgaskl@gmail.com>
2017-04-28 23:35:59 -0600

    Clean up devise routes
    Routes were in spanish, for consistency everything must be in english

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
   root 'defaults#index'
 
-  devise_for :users, skip: [:sessions]
-  as :user do
-    get 'ingreso', to: 'devise/sessions#new', as: :new_user_session
-    post 'ingreso', to: 'devise/sessions#create', as: :user_session
-    delete 'salir', to: 'devise/sessions#destroy', as: :destroy_user_session
-  end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
 end
