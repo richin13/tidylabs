29446f3ffdf45d89dc0e119a2c90f1aea0f99ec9
Dascr32 <dasgaskl@gmail.com>
2017-05-26 16:44:15 -0600

    Remove faker gem
    Faker was slowing down the tests and was used only by one factory.

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    email { Faker::Internet.safe_email('john') }
-    password Faker::Internet.password
+    email { 'john_doe@gmail.com' }
+    password '123456'
   end
 end
