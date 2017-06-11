0429f6bccc1712b3c0eed569ee8e0f3dc3f79f04
Dascr32 <dasgaskl@gmail.com>
2017-06-11 02:12:40 -0600

    Make app title customizable
    Allows the app title to be customized from any view.
    Use ```<% title 'Your title here' %>``` in the view to set the custom
    title.
    
    When a custom title isn't specified in the view, the default title is
    shown: "Oficina De Computo - UCR | Tydilabs".
    
    When a title is specified, it will be changed to:
    "Oficina De Computo - UCR | <your title here>"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
   def code_as_svg(id_code, size = 4)
     raw generate_qr_code(id_code).as_svg(module_size: size)
   end
