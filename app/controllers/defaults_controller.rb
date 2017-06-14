0caffb8f9922b20fa541e9ab7ac27f24e970b26a
Ricardo Madriz <richin13@gmail.com>
2017-06-14 10:11:03 -0600

    Improve index page design

--- app/controllers/defaults_controller.rb
+++ app/controllers/defaults_controller.rb
@@ -2,5 +2,16 @@ class DefaultsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @blocks = [
+        {'url': areas_path, 'name': 'Áreas', 'icon': 'building'},
+        {'url': assets_url, 'name': 'Activos', 'icon': 'desktop'},
+        {'url': revisions_url, 'name': 'Revisiones', 'icon': 'calendar-check-o'},
+        {'url': '', 'name': 'Préstamos', 'icon': 'handshake-o'},
+        {'url': '', 'name': 'Mantenimiento', 'icon': 'ticket'},
+        {'url': users_path, 'name': 'Colaboradores', 'icon': 'users'},
+        {'url': '', 'name': 'Móvil', 'icon': 'android'},
+        {'url': '', 'name': 'Reportes', 'icon': 'file-pdf-o'},
+        {'url': '', 'name': 'Ayuda', 'icon': 'support'},
+    ]
   end
 end
