7dd7fd781325b8050312268af7e0c356650ebddf
Dascr32 <dasgaskl@gmail.com>
2017-06-16 23:49:19 -0600

    Improve mobile app download page
    Minor touches here and there

--- app/controllers/defaults_controller.rb
+++ app/controllers/defaults_controller.rb
@@ -3,23 +3,24 @@ class DefaultsController < ApplicationController
 
   def index
     @blocks = [
-        {'url': areas_path, 'name': 'Áreas', 'icon': 'building'},
-        {'url': assets_path, 'name': 'Activos', 'icon': 'desktop'},
-        {'url': revisions_path, 'name': 'Revisiones', 'icon': 'calendar-check-o'},
-        {'url': loans_path, 'name': 'Préstamos', 'icon': 'handshake-o'},
-        {'url': '', 'name': 'Mantenimiento', 'icon': 'ticket'},
-        {'url': users_path, 'name': 'Colaboradores', 'icon': 'users'},
-        {'url': '', 'name': 'Reportes', 'icon': 'file-pdf-o'},
-        {'url': app_path, 'name': 'Móvil', 'icon': 'android'},
-        {'url': help_path, 'name': 'Ayuda', 'icon': 'support'},
+      { url: areas_path, name: 'Áreas', icon: 'building' },
+      { url: assets_path, name: 'Activos', icon: 'desktop' },
+      { url: revisions_path, name: 'Revisiones', icon: 'calendar-check-o' },
+      { url: loans_path, name: 'Préstamos', icon: 'handshake-o' },
+      { url: root_path, name: 'Mantenimiento', icon: 'ticket' },
+      { url: users_path, name: 'Colaboradores', icon: 'users' },
+      { url: root_path, name: 'Reportes', icon: 'file-pdf-o' },
+      { url: app_path, name: 'Móvil', icon: 'android' },
+      { url: help_path, name: 'Ayuda', icon: 'support' },
     ]
   end
 
   def app
+    @screenshots = Dir.glob("app/assets/images/mobile_app/*.jpg")
   end
 
   def download
-    send_file("#{Rails.root}/public/Tidylabs.apk", filename: 'Tidylabs.apk')
+    send_file("public/Tidylabs.apk", filename: 'Tidylabs.apk')
   end
 
   def help
